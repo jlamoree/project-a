bcbff03891dd02e828ccd00bca6515135cee3d89
Joseph Lamoree <joseph@lamoree.com>
2022-06-02 07:12:46 -0700

    Updated the "build" to show all the drivers in project-b

--- build.sh
+++ build.sh
@@ -4,9 +4,7 @@ echo "Building the project..."
 
 if [ -d "project-b" ]; then
   echo "The following files exist in project-b"
-  for f in $(ls -1 "project-b"); do
-    echo "$f"
-  done
+  ls -lR "project-b"
 else
   echo "No such thing as project-b"
 fi
